f20d1049f0dc1ad2fda073b3669b724567d43bc5
Markus Kohlmeyer <1191804+JoeUser78@users.noreply.github.com>
2025-09-06 15:01:34 +0200

    Some fixes

--- .puppeteerScript.js
+++ .puppeteerScript.js
@@ -1,5 +1,5 @@
 module.exports = async (page, context) => {
-  console.log("Hello, this is AnupamAS01!");
+  console.log("PuppeTeer is ready.");
   // LHCI gibt hier nur ein minimales Objekt, kein echtes Puppeteer-Page.
   // Deshalb keine page.type(), page.click() oder page.evaluate() möglich.
   return;
