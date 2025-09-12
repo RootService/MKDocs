f559208951d754943e23330c4a55d77193e2493c
JoeUser78 <1191804+JoeUser78@users.noreply.github.com>
2025-09-13 01:16:30 +0200

    Some fixes

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -4,13 +4,10 @@ module.exports = {
   rules: {
     // moderne Notation: rgb() ohne Kommas, Alpha als Prozent
     'color-function-notation': 'modern',
-    'color-function-alias-notation': true,         // rgba()/hsla() -> rgb()/hsl()
-    'alpha-value-notation': 'percentage',          // z.B. 12% statt 0.12
-    'color-hex-length': 'short',                   // #f6c statt #ff66cc (optional)
+    'color-function-alias-notation': true, // rgba()/hsla() -> rgb()/hsl()
+    'alpha-value-notation': 'percentage', // z.B. 12% statt 0.12
+    'color-hex-length': 'short', // #f6c statt #ff66cc (optional)
     // BEM + MkDocs-Klassen erlauben: foo, foo__el, foo--mod
-    'selector-class-pattern': [
-      '^[a-z0-9]+(?:-[a-z0-9]+)*(?:__(?:[a-z0-9]+(?:-[a-z0-9]+)*))?(?:--[a-z0-9]+(?:-[a-z0-9]+)*)?$',
-      { resolveNestedSelectors: true }
-    ],
+    'selector-class-pattern': ['^[a-z0-9]+(?:-[a-z0-9]+)*(?:__(?:[a-z0-9]+(?:-[a-z0-9]+)*))?(?:--[a-z0-9]+(?:-[a-z0-9]+)*)?$', { resolveNestedSelectors: true }],
   },
 };
